faa8e75e754b1f101992829fa41af6857ab5b5df
lucas-thompson1104 <128449890+lucas-thompson1104@users.noreply.github.com>
2023-10-10 05:51:39 +0000

    added firefox

--- app/run.sh
+++ app/run.sh
@@ -1,2 +1,3 @@
-docker build -t novnc-application .
-docker run --rm -d --shm-size 2g --name vnc-container -p 8080:80 novnc-application
+clear
+docker build -t vnc-app .
+docker run --rm -d --shm-size 2g --name vnc-container -p 8080:80 vnc-app
