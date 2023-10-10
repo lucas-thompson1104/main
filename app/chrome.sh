ca768ecaecb288bf19dcb37bf29a098dc156ddd8
lucas-thompson1104 <128449890+lucas-thompson1104@users.noreply.github.com>
2023-10-10 06:21:51 +0000

    nothing

---
+++ app/chrome.sh
@@ -0,0 +1,2 @@
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
+apt install -y ./google-chrome-stable_current_amd64.deb
--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,4 @@
 clear
 docker build -t vnc-app .
 docker run --rm -d --shm-size 2g --name vnc-container -p 8080:80 vnc-app
+docker exec -it vnc-container bash
